9093951f5b3240c229db7416f6a8cef8696d41f3
Ross Gardler <rgardler@apache.org>
2019-01-16 22:36:57 -0800

    Add Rain Maker plugin packager

---
+++ Assets/Digital Painting/Plugins/Weather_RainMaker/Scripts/Editor/RainMakerPackageBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RainMakerPackageBuilder : PackageBuilder {
+
+    [MenuItem("Digital Painting/Build/Build Rain Maker Package")]
+    new public static void Build()
+    {
+        string pluginName = "Weather_RainMaker";
+        string rootDir = "Assets/Digital Painting/Plugins/" + pluginName;
+        string packageName = pluginName + ".unitypackage";
+
+        AssetDatabase.ExportPackage(rootDir, packageName, ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+        Debug.Log("Exported " + packageName);
+    }
+}
