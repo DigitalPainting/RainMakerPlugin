99ddc9c817cdbd01d40097445b1fdab47927ddfc
Ross Gardler <rgardler@apache.org>
2019-01-18 21:48:14 -0800

    Write against the latest core code

--- Assets/Digital Painting/Plugins/Weather_RainMaker/Scripts/Editor/RainMakerPackageBuilder.cs
+++ Assets/Digital Painting/Plugins/Weather_RainMaker/Scripts/Editor/RainMakerPackageBuilder.cs
@@ -7,13 +7,14 @@ public class RainMakerPackageBuilder : PackageBuilder {
     [MenuItem("Digital Painting/Build/Build RainMaker Plugin")]
     new public static void Build()
     {
-
         string rootDir = "Assets\\Digital Painting\\Plugins\\Weather_RainMaker";
-        string excludeSubDir = "";
         string packageName = "DigitalPainting.unitypackage";
 
-        DeleteAllButPackages(rootDir, excludeSubDir);
-        BuildPackage(rootDir, packageName);
-        ReinstateFiles(rootDir, excludeSubDir);
+        MoveExcludedFiles(rootDir);
+
+        AssetDatabase.ExportPackage(rootDir, packageName, ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+        Debug.Log("Exported " + packageName);
+
+        RecoverExcludedFiles(rootDir);
     }
 }
